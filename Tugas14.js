80fd30135f80f2fe2a437b7c5d60cbed9d6eeabe
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-09-07 03:40:27 +0700

    Tugas 14 javascript Niomic

---
+++ Tugas14.js
@@ -0,0 +1,11 @@
+let angka = 37;
+let tanggal = 27;
+
+let hasil = angka/tanggal;
+console.log(hasil);
+//cell
+console.log(Math.ceil(hasil));
+//floor
+console.log(Math.floor(hasil));
+//round
+console.log(Math.round(hasil));
